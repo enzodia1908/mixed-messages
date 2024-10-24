295a6c8cfae95a04e287f4137a2b0d4d31aef325
Nadia Banik <nadiabanik@hotmail.com>
2024-10-24 12:13:19 +1100

    Add future events object

---
+++ script.js
@@ -0,0 +1,30 @@
+// object to group arrays by event types and event data
+const futureEvents = {
+    days: ['Next week','Tomorrow', 'Tonight', 'Three days from now'],
+    times: ['In five minutes', 'In two hours', 'In one hour', 'In 30 minutes'],
+    moods: ['happily', 'excitedly', 'anxiously', 'sadly'],
+    event1: [
+        'communicate with aliens through interpretive dance', 
+        'forget where you parked', 
+        'befriend a talking cat',
+        'accidentally time travel'],
+    event2: [
+        'win a hotdog eating contest',
+        'build a sandcastle',
+        'eat tacos with a dragon',
+        'lose your shoes'
+    ],
+    event3: [
+        'win a lifetime supply of random ducks',
+        'meet a wizard in a coffee shop', 
+        'find a portal in your sock drawer',
+        'wake up as a penguin'
+    ],
+    locations: [
+        'in outer space',
+        'on top of a giant marshmallow',
+        'at a theme park run by robots',
+        'on a planet inhabited by sentient pizza toppings.'
+    ]
+}
+
